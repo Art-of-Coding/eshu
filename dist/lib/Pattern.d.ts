0f81607ad00cac1962db9b0692c5d227718edbbb
Michiel van der Velde <michiel@michielvdvelde.nl>
2017-07-07 16:40:19 +0200

    Remove dist/

--- dist/lib/Pattern.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IParams {
-    [x: string]: any;
-}
-export declare function clean(pattern: string | string[]): string | string[];
-export declare function cleanSingle(pattern: string): string;
-export declare function cleanMulti(patterns: string[]): string[];
-export declare function matches(pattern: string, topic: string): boolean;
-export declare function extract(pattern: string, topic: string): IParams;
